a8d1c28c3fa3354160efbfc94da5ddf4379d96b0
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-30 17:46:23 +0330

    update

--- TODO/main.py
+++ TODO/main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -13,6 +16,12 @@ def get_db() :
     finally:
         db.close()
 
+class Todo(BaseModel):
+    title: str
+    description:  Optional[str]
+    priority: int = Field(gt=0, lt=6, description="The priority must be between1-5")
+    complete: bool
+
 models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
@@ -26,6 +35,24 @@ async def read_todo(todo_id: int, db: Session =Depends(get_db)):
     if todo_model is not None:
         return todo_model
     raise http_exception()
+
+@app.post("/")
+async def create_db(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.complete = todo.complete
+    todo_model.priority = todo.priority
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        'transaction': 'Successful'
+    }
+
 
 def http_exception() :
     return HTTPException(status_code=404, detail="Todo Not Found!")
