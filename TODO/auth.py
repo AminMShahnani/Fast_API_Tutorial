fbbd99e873648f77a75d14487e26c16e9f5ab8a1
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-30 23:26:07 +0330

    Create auth.py

---
+++ TODO/auth.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import models
+
+class CreateUser(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+app = FastAPI()
