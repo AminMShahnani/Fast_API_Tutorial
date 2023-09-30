575b24990e0af467a34a4fb8ed264160da017337
aminmshahnani <mshahnani.amin@gmail.com>
2023-09-30 22:26:41 +0330

    update

--- TODO/main.py
+++ TODO/main.py
@@ -48,8 +48,40 @@ async def create_db(todo: Todo, db: Session = Depends(get_db)):
     db.add(todo_model)
     db.commit()
 
+    return successful_response(201)
+
+@app.put("/{todo_id}")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        return http_exception()
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.complete = todo.complete
+    todo_model.priority = todo.priority
+
+    db.add(todo_model)
+    db.commit()
+
+    return successful_response(200)
+
+@app.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None :
+        return http_exception()
+    
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+
+    return successful_response(200)
+
+
+def successful_response(status_code: int) :
     return {
-        'status': 201,
+        'status': status_code,
         'transaction': 'Successful'
     }
 
